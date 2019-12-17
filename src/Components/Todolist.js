158239e5ec4e282b023e4164f8308e53d8ae5b56
ArupDawn <arupchandradawn@gmail.com>
2019-12-17 22:55:37 +0530

    Third Commit

--- src/Components/Todolist.js
+++ src/Components/Todolist.js
@@ -4,24 +4,60 @@ class Todolist extends Component {
 
     constructor(props){
         super(props);
-
-          this.state = {
-              arr:['Morning Walk','Going to Gym','Client Meeting'],
-              textVal:''
+        if(localStorage.getItem('toDO')){
+            let a=JSON.parse(localStorage.getItem('toDO'));
+            this.state = {
+                arr:[...a],
+                textVal:''
+            }
+          }
+          else{
+            this.state = {
+                arr:['Morning Walk','Going to Gym','Client Meeting'],
+                textVal:''
+            }
           }
     }
         
     
-
+    onChangeText(e){
+        this.setState({
+            textVal:e.target.value
+        })
+    }
+  
+    
+  
+    AddE(e){
+      if(e.charCode === 13){
+          e.preventDefault();
+          let newarr = this.state.arr;
+          newarr.push(this.state.textVal);
+  
+          this.setState({
+              textVal:'',
+              arr:[...newarr]
+          })
+  
+          localStorage.setItem('toDO',JSON.stringify(newarr));
+      }
+    }
 
   render() {
+    var a = localStorage.getItem('toDO');
+    var aa= JSON.stringify(a);
+    console.log('Data ='+a);
+    if(! localStorage.getItem('toDO')){
+        localStorage.setItem('toDO',JSON.stringify(this.state.arr));
+    }
+
     return (
       <div className="App2">
 
         <header className="App-header">
           <div className="App-title">ToDo List...
-            <button type="button" className="btn">
-                <i className="fas fa-plus-square"></i>
+            <button type="button" className="btn" data-toggle="collapse" 
+                data-target={"#copp"}><i className="fas fa-plus-square"></i>
             </button>
           </div>
         </header>
@@ -41,7 +77,13 @@ class Todolist extends Component {
                 )
                 })
             }
-                
+                <div id="copp" className="collapse">
+                        <i className="fas fa-dot-circle"></i>{" "}{}
+                        <input type="text" value={this.state.textVal} placeholder="Enter your list items"
+                        onChange={this.onChangeText.bind(this)} onKeyPress={this.AddE.bind(this)} required/>
+                        
+                        <hr></hr>
+                </div>
             </div>
 
 
