f4e593de9591aad11b2b4fa9dd12574364b75357
ArupDawn <arupchandradawn@gmail.com>
2019-12-17 22:40:25 +0530

    Second Commit

--- src/Components/Todolist.js
+++ src/Components/Todolist.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
 
 class Todolist extends Component {
+
+    constructor(props){
+        super(props);
+
+          this.state = {
+              arr:['Morning Walk','Going to Gym','Client Meeting'],
+              textVal:''
+          }
+    }
+        
+    
+
+
   render() {
     return (
       <div className="App2">
 
         <header className="App-header">
           <div className="App-title">ToDo List...
-            <button type="button" className="btn">+
+            <button type="button" className="btn">
+                <i className="fas fa-plus-square"></i>
             </button>
           </div>
         </header>
         
-        <div>
+        <div className="MainContent">
+
+            <div className="MainList">
+            {
+                this.state.arr.map((item,i)=>{
+                    return (
+
+                    <div>
+                        <i className="fas fa-dot-circle"></i>{}{" "}{item}
+                    <hr></hr>
+                    </div>
+                    
+                )
+                })
+            }
+                
+            </div>
+
+
         </div>
 
       </div>
